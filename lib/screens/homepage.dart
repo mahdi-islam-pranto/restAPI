b236eb46ab010800e70dddc49dbb7b6d4c65149a
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2024-09-05 19:20:28 +0600

    task api complete

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'taskPage.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,80 +13,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Tasks> allTasksList = [];
-
-  Future<List<Tasks>> fetchTasks() async {
-    final response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
-    var data = jsonDecode(response.body.toString());
-
-    if (response.statusCode == 200) {
-      for (Map i in data) {
-        Tasks tasks = Tasks(
-          id: i['id'],
-          title: i['title'],
-          completed: i['completed'],
-          userId: i['userId'],
-        );
-        allTasksList.add(tasks);
-        print(tasks.title);
-      }
-      return allTasksList;
-    } else {
-      throw Exception('Failed to load tasks');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: const Center(
-            child: Text('All Tasks',
+            child: Text('Discover API',
                 style: TextStyle(fontWeight: FontWeight.bold))),
       ),
-      body: Column(
-        children: [
-          //Show all tasks
-          Expanded(
-            child: FutureBuilder(
-              future: fetchTasks(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: allTasksList.length,
-                    itemBuilder: (context, index) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            "${snapshot.data![index].id}. Title: ${snapshot.data![index].title} ",
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 5,
-                          ),
-                          Builder(
-                            builder: (context) {
-                              if (snapshot.data![index].completed) {
-                                return Icon(Icons.check_box);
-                              }
-
-                              return Icon(Icons.check_box_outline_blank);
-                            },
-                          ),
-                          Text("User Id: ${snapshot.data![index].userId}"),
-                        ],
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            ),
+      body: Center(
+        child:
+            //Show all tasks
+            Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Taskpage(),
+                        ));
+                  },
+                  child: const Text("Task API")),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/screens/taskPage.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Taskpage extends StatefulWidget {
+  const Taskpage({super.key});
+
+  @override
+  State<Taskpage> createState() => _TaskpageState();
+}
+
+class _TaskpageState extends State<Taskpage> {
+  List<Tasks> allTasksList = [];
+
+  Future<List<Tasks>> fetchTasks() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+    var data = jsonDecode(response.body.toString());
+
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        Tasks tasks = Tasks(
+          id: i['id'],
+          title: i['title'],
+          completed: i['completed'],
+          userId: i['userId'],
+        );
+        allTasksList.add(tasks);
+      }
+      return allTasksList;
+    } else {
+      throw Exception('Failed to load tasks');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  // refresh the list
+                  fetchTasks();
+                });
+              },
+              icon: const Icon(Icons.refresh))
+        ],
+        backgroundColor: Colors.green,
+        title: const Center(
+            child: Text('All Tasks',
+                style: TextStyle(fontWeight: FontWeight.bold))),
+      ),
+      body: Column(
+        children: [
+          //Show all tasks
+          Expanded(
+            child: FutureBuilder(
+              future: fetchTasks(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: allTasksList.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${snapshot.data![index].id}. Title: ${snapshot.data![index].title} ",
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 5,
+                          ),
+                          Builder(
+                            builder: (context) {
+                              if (snapshot.data![index].completed) {
+                                return const Icon(Icons.check_box);
+                              }
+
+                              return const Icon(Icons.check_box_outline_blank);
+                            },
+                          ),
+                          Text("User Id: ${snapshot.data![index].userId}"),
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Tasks {
+  int id;
+  String title;
+  bool completed;
+  int userId;
+
+  Tasks(
+      {required this.id,
+      required this.title,
+      required this.completed,
+      required this.userId});
+}
